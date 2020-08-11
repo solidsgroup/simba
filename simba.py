cf393ee8a5c0aa7c1c2269b321abc05bc53f22db
Brandon Runnels <brunnels@uccs.edu>
2020-08-11 08:51:58 -0600

    look for .simba

--- simba.py
+++ simba.py
@@ -1,1 +1,23 @@
-print("This is simba")
+import pathlib
+import argparse
+import src.record
+
+#
+# Look recursively for a .simba directory in this or a parent path
+#
+def getSimbaDir(path):
+    if (path/".simba").is_dir():
+        return (path/".simba").absolute()
+    else:
+        if (path == path.parent):
+            raise(Exception("No .simba directory found"))
+        else:
+            return(getSimbaDir(path.parent))
+simbaPath = getSimbaDir(pathlib.Path.cwd())
+print(simbaPath)
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument("mode")
+#args = parser.parse_args()
+
+
