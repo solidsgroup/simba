9ea4ebd1a450aa0e5b29963d48d7fd44d1ad89b8
Brandon Runnels <brunnels@uccs.edu>
2020-12-16 11:50:40 -0700

    rm dox

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,8 @@ version = u''
 release = u''
 
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-if read_the_docs_build:
-    subprocess.call('cd ..; doxygen', shell=True)
+#if read_the_docs_build:
+#    subprocess.call('cd ..; doxygen', shell=True)
 
 
 # -- General configuration ---------------------------------------------------
